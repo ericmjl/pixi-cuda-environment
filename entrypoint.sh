e91facee2a1101f2fa79c53cc6ebe9a8f8effaf4
Eric Ma <ericmajinglong@gmail.com>
2024-07-29 07:17:08 -0400

    Add documentation on entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# Taken from: https://stackoverflow.com/a/44079215
-# Check for NVIDIA hardware
+# Modified from: https://stackoverflow.com/a/44079215
+# This script ensures that within a Docker container
+# we have the right pixi environment activated
+# before executing a command
+
+# If `nvidia-smi` is available, then execute command in the `cuda` environment
+# as defined in `pyproject.toml`.
 if command -v nvidia-smi &> /dev/null; then
     pixi shell -e cuda
 else
